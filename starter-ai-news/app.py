82a9961c75d3c6a1f28e0aad432ad5afd0277263
tjenkins47 <30880820+tjenkins47@users.noreply.github.com>
2025-07-24 15:45:57 -0400

    Add files via upload

--- starter-ai-news/app.py
+++ starter-ai-news/app.py
@@ -20,6 +20,3 @@ def api_news():
         "categories": ["Model"]
     }
     return jsonify([story])
-
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
